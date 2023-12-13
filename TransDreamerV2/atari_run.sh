2ce89d037dc1c2cb4346b33abf1b03ac95f630f6
Xiaona Zhou <xzhou1@plan.cs.vt.edu>
2023-12-12 16:57:32 -0800

    first commit

---
+++ TransDreamerV2/atari_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#conda remove -n dreamerv3 --all
+source activate trans_dreamer
+nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free --format=csv -l 60 > $SLURM_JOBID.img.gpu.log &
+echo ${SLURM_STEP_GPUS:-$SLURM_JOB_GPUS}
+ 
+ 
+CUDA_VISIBLE_DEVICES=7 python main.py --config-file config_files/configs_atari_pong.py
+
